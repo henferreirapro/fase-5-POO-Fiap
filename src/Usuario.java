57aa75d0989220822d3749a6ab19e797f1a60f6d
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2024-08-25 21:01:02 -0300

    Create classes Usuario UsuarioTeste

---
+++ src/Usuario.java
@@ -0,0 +1,29 @@
+
+
+public class Usuario {
+
+  private String nomeUsuario;
+  private String email;
+  private String senha;
+
+  // Iniciando o construtor
+  public Usuario() {}
+
+  // add parametros no construtor
+  public Usuario(String nomeUsuario, String email, String senha) {
+    this.nomeUsuario = nomeUsuario;
+    this.email = email;
+    this.senha = senha;
+  }
+
+  // Executando método da mensagem
+  public void msgRegistro() {
+    System.out.printf("%nUsuario Registrado com Sucesso, prazer em te conhecer %s! %n", nomeUsuario);
+
+    System.out.println("Enviamos um e-mail de confirmação para o e-mail: " + email);
+
+    // a senha só será mostrada aqui para fins de visualização.
+    System.out.printf("Senha salva com Sucesso: %s %n", senha);
+  }
+}
+
---
+++ src/UsuarioTeste.java
@@ -0,0 +1,11 @@
+public class UsuarioTeste {
+  public static void main(String[] args) {
+    
+    // Criando objeto Usuario
+    Usuario usuario = new Usuario("Henrique", "henrique.teste@gmail.com", "EsqueciMinhaSenha123");
+
+    // Exibindo os dados do usuario
+    usuario.msgRegistro();
+    
+  }
+}
