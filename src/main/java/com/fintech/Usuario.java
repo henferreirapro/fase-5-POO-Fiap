1c2de2aa9f5b6c4dc1ef4264ada8f580d52f949a
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2024-09-09 21:56:58 -0300

    Update Usuario.java
    Herança: A classe Usuario herda de Pessoa, aproveitando os atributos e métodos comuns, como exibirDados().
    
    Sobrescrita de Método (@Override): A classe Usuario sobrescreve o método exibirDados() para incluir a exibição da senha.
    
    Construtores: Os construtores da classe Usuario utilizam o construtor da superclasse (super()).

--- src/main/java/com/fintech/Usuario.java
+++ src/main/java/com/fintech/Usuario.java
@@ -1,27 +1,44 @@
 package main.java.com.fintech;
 
 
-public class Usuario {
+public class Usuario extends Pessoa {
 
-  private String nomeUsuario;
-  private String email;
   private String senha;
+
 
   // Iniciando o construtor
   public Usuario() {}
 
   // add parametros no construtor
   public Usuario(String nomeUsuario, String email, String senha) {
-    this.nomeUsuario = nomeUsuario;
-    this.email = email;
+    super(nomeUsuario, email);
+    this.senha = senha;
+    
+  }
+
+
+  // add get e set da senha do usuario
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
     this.senha = senha;
   }
 
+  // add metodo exibir os dados de uma pessoa
+  @Override
+  public void exibirDadosPessoa() {
+    super.exibirDadosPessoa();
+    System.out.printf("Senha do usuario: %s %n", senha);
+  }
+
+
   // Executando método da mensagem
   public void msgRegistro() {
-    System.out.printf("%nUsuario Registrado com Sucesso, prazer em te conhecer %s! %n", nomeUsuario);
+    System.out.printf("%nUsuario Registrado com Sucesso, prazer em te conhecer %s! %n", getNomeUsuario());
 
-    System.out.println("Enviamos um e-mail de confirmação para o e-mail: " + email);
+    System.out.println("Enviamos um e-mail de confirmação para o e-mail: " + getEmail());
 
     // a senha só será mostrada aqui para fins de visualização.
     System.out.printf("Senha salva com Sucesso: %s %n", senha);
